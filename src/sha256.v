184c7e000ad2cd5457b858976c2ab370aafe472c
warriorjacq9-no2fa <jflusche3180@student.pps.net>
2026-02-11 17:13:53 +0000

    Optimize start logic

--- src/sha256.v
+++ src/sha256.v
@@ -24,17 +24,6 @@ module sha256 (
     output reg [255:0] hash,
     output reg done
 );
-    /* Run logic */
-    reg run;
-    always @(posedge clk or negedge rst_n) begin
-        if(!rst_n) begin
-            run <= 1'b0;
-        end else begin
-            if(start) run <= 1'b1;
-            else if(done) run <= 1'b0;
-        end
-    end
-
     `define rotr(x, n) (((x) >> (n)) | ((x) << (32 - (n))))
     wire [31:0] ch_e = (e & f) ^ (~e & g);
     wire [31:0] maj_a = (a & b) ^ (a & c) ^ (b & c);
@@ -86,7 +75,7 @@ module sha256 (
     end
     reg [255:0] int_hash;
     // State machine
-    localparam S_SCHEDULE=0, S_INIT=1, S_COMPUTE=2;
+    localparam S_IDLE=0, S_INIT=1, S_COMPUTE=2;
     localparam S_OUT=3, S_NEXT=4, S_DONE=5;
     reg [4:0] state;
     // To track iteration for 640-bit double-SHA
@@ -99,7 +88,7 @@ module sha256 (
     always @(posedge clk or negedge rst_n) begin
         if(!rst_n) begin
             // State variables
-            state <= S_SCHEDULE;
+            state <= S_IDLE;
             iteration <= I_BLOCK1;
             i <= 6'b0;
             int_hash <= 256'b0;
@@ -115,16 +104,10 @@ module sha256 (
             H6 <= CH6;
             H7 <= CH7;
         end else begin
-            if(run) begin
-                if(state == S_SCHEDULE) begin
-                    if(i < 16) begin
-                        W[i[3:0]] <= Wnext;
-                        i <= i + 1;
-                    end else begin
-                        i <= 0;
-                        state <= S_INIT;
-                    end
-                end else if(state == S_INIT) begin
+            if(state == S_IDLE) begin
+                if(start) state <= S_INIT;
+            end else if(state == S_INIT) begin
+                if(i == 0) begin
                     a <= H0;
                     b <= H1;
                     c <= H2;
@@ -133,74 +116,80 @@ module sha256 (
                     f <= H5;
                     g <= H6;
                     h <= H7;
+                end
+                if(i < 16) begin
+                    W[i[3:0]] <= Wnext;
+                    i <= i + 1;
+                end else begin
+                    i <= 0;
                     state <= S_COMPUTE;
-                end else if(state == S_COMPUTE) begin
-                    h <= g;
-                    g <= f;
-                    f <= e;
-                    e <= d + t1;
-                    d <= c;
-                    c <= b;
-                    b <= a;
-                    a <= t1 + t2;
+                end
+            end else if(state == S_COMPUTE) begin
+                h <= g;
+                g <= f;
+                f <= e;
+                e <= d + t1;
+                d <= c;
+                c <= b;
+                b <= a;
+                a <= t1 + t2;
 
-                    if(i >= 16) begin
-                        W[0] <= W[1];
-                        W[1] <= W[2];
-                        W[2] <= W[3];
-                        W[3] <= W[4];
-                        W[4] <= W[5];
-                        W[5] <= W[6];
-                        W[6] <= W[7];
-                        W[7] <= W[8];
-                        W[8] <= W[9];
-                        W[9] <= W[10];
-                        W[10] <= W[11];
-                        W[11] <= W[12];
-                        W[12] <= W[13];
-                        W[13] <= W[14];
-                        W[14] <= W[15];
-                        W[15] <= Wt;
-                    end
+                if(i >= 16) begin
+                    W[0] <= W[1];
+                    W[1] <= W[2];
+                    W[2] <= W[3];
+                    W[3] <= W[4];
+                    W[4] <= W[5];
+                    W[5] <= W[6];
+                    W[6] <= W[7];
+                    W[7] <= W[8];
+                    W[8] <= W[9];
+                    W[9] <= W[10];
+                    W[10] <= W[11];
+                    W[11] <= W[12];
+                    W[12] <= W[13];
+                    W[13] <= W[14];
+                    W[14] <= W[15];
+                    W[15] <= Wt;
+                end
 
-                    if(i == 63) state <= S_OUT;
-                    else i <= i + 1;
-                    
-                end else if(state == S_OUT) begin
-                    H0 <= a + H0;
-                    H1 <= b + H1;
-                    H2 <= c + H2;
-                    H3 <= d + H3;
-                    H4 <= e + H4;
-                    H5 <= f + H5;
-                    H6 <= g + H6;
-                    H7 <= h + H7;
-                    state <= S_NEXT;
-                end else if(state == S_NEXT) begin
-                    if(iteration == I_BLOCK1) begin // Finished first block
-                        i <= 0;
-                        iteration <= I_BLOCK2;
-                        state <= S_SCHEDULE;
-                    end else if(iteration == I_BLOCK2) begin // Finished second block
-                        int_hash <= {H0, H1, H2, H3, H4, H5, H6, H7};
-                        // Reset SHA256 state and start new hash
-                        H0 <= CH0;
-                        H1 <= CH1;
-                        H2 <= CH2;
-                        H3 <= CH3;
-                        H4 <= CH4;
-                        H5 <= CH5;
-                        H6 <= CH6;
-                        H7 <= CH7;
-                        i <= 0;
-                        state <= S_SCHEDULE;
-                        iteration <= I_DOUBLE;
-                    end else if(iteration == I_DOUBLE) begin
-                        hash <= {H0, H1, H2, H3, H4, H5, H6, H7};
-                        done <= 1;
-                        iteration <= I_DONE;
-                        state <= S_DONE;
-                    end
+                if(i == 63) state <= S_OUT;
+                else i <= i + 1;
+                
+            end else if(state == S_OUT) begin
+                H0 <= a + H0;
+                H1 <= b + H1;
+                H2 <= c + H2;
+                H3 <= d + H3;
+                H4 <= e + H4;
+                H5 <= f + H5;
+                H6 <= g + H6;
+                H7 <= h + H7;
+                state <= S_NEXT;
+            end else if(state == S_NEXT) begin
+                if(iteration == I_BLOCK1) begin // Finished first block
+                    i <= 0;
+                    iteration <= I_BLOCK2;
+                    state <= S_INIT;
+                end else if(iteration == I_BLOCK2) begin // Finished second block
+                    int_hash <= {H0, H1, H2, H3, H4, H5, H6, H7};
+                    // Reset SHA256 state and start new hash
+                    H0 <= CH0;
+                    H1 <= CH1;
+                    H2 <= CH2;
+                    H3 <= CH3;
+                    H4 <= CH4;
+                    H5 <= CH5;
+                    H6 <= CH6;
+                    H7 <= CH7;
+                    i <= 0;
+                    state <= S_INIT;
+                    iteration <= I_DOUBLE;
+                end else if(iteration == I_DOUBLE) begin
+                    hash <= {H0, H1, H2, H3, H4, H5, H6, H7};
+                    done <= 1;
+                    iteration <= I_BLOCK1;
+                    state <= S_IDLE;
                 end
             end
         end
